bdc557662a1f560c2bae53de9e61910b2a0c036a
islamicanalytics5 <islamicanalytics5@gmail.com>
2025-03-12 22:43:11 +0800

    Added Postgres db

--- core/settings.py
+++ core/settings.py
@@ -92,8 +92,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ROfREdFQJikVdbwOFrQdVdtnvJNuDTLT',
+        # 'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': 'switchyard.proxy.rlwy.net',
+        'PORT': '39831',
     }
 }
 
